6ab4a0fb4e667b1e105cffd19e2b3219b155c8c5
Chung-I <leon129506@gmail.com>
2019-07-26 09:50:48 +0800

    enable joint vocabulary creation and training

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
-export MODEL_PATH="phoneme"
-#allennlp make-vocab -s $VOCAB_PATH --include-package stt $1
-python3 -W ignore run.py train $1 -s $MODEL_PATH --include-package stt
+export MODEL_PATH=$3
+export VOCAB_PATH="phoneme"
+allennlp make-vocab -s $VOCAB_PATH --include-package stt $1
+python3 -W ignore run.py train $2 -s $MODEL_PATH --include-package stt
