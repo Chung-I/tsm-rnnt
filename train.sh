a9f016416af326a1693707478392a32c1c244d66
Chung-I <leon129506@gmail.com>
2019-10-01 11:32:40 +0800

    try to ignore train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export FISHER_PATH="data/fisher_callhome_spanish/translation"
-export LEXICON_PATH="/groups/nlpmaster/lexicon.txt"
+export LEXICON_PATH="/home/nlpmaster/lexicon.txt"
 export FISHER_TRAIN_PATH="data/fisher_callhome_spanish/s5/data/train/feats.scp"
 export FISHER_VAL_PATH="data/fisher_callhome_spanish/s5/data/dev/feats.scp"
 export FISHER_VOCAB_PATH="data/fisher_callhome_spanish/vocabulary"
